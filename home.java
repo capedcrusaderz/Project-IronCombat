5fe01bec9683c0fed7a6db50b6272cf4a84bde1b
Shyam Crusader <capedcrusaderzofficial@gmail.com>
2017-04-05 10:14:06 +0530

    updated v1.2
    sum changed to product of 2 numbers.

--- home.java
+++ home.java
@@ -2,13 +2,13 @@ class home
 {
 public static void main(String args[])
 {
-System.out.println("Sum of two numbers");
+System.out.println("Product of two numbers");
   int a;
   int b;
   a=10;
   b=20;
   int c;
-  c=a+b;
-  System.out.println("Sum of"+a+"and"+b+"is"+c);
+  c=a*b;
+  System.out.println("Product of"+a+"and"+b+"is"+c);
 }
 }
